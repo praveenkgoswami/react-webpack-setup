9f9ca6443747d494c22a5267105b0739ec34d57a
Praveen <praveen.kumar@tothenew.com>
2020-06-05 11:58:55 +0530

    port updated

--- webpack.config.js
+++ webpack.config.js
@@ -36,5 +36,10 @@ module.exports = {
       template: "./src/index.html",
       filename: "./index.html"
     })
-  ]
+  ],
+  devServer: {
+    // contentBase: path.join(__dirname, 'dist'),
+    // compress: true,
+    port: 9000
+  }
 };
