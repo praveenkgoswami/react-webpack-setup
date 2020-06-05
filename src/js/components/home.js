8d5ede2eeb390303e3adf7c9652b56a35ca036a4
Praveen <praveen.kumar@tothenew.com>
2020-06-05 12:17:24 +0530

    home created

---
+++ src/js/components/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h1 className="t-center">Home</h1>
+      <p>Webpack setup</p>
+    </div>
+  )
+}
+
+export default Home;
